49117cdf73d43c87b7c27431a9f0ca643df5ce1a
codingchili <rduda@kth.se>
2018-11-01 19:26:21 +0100

    more curses.

--- cli.py
+++ cli.py
@@ -2,11 +2,15 @@ import curses
 import curses.textpad as textpad
 from curses import wrapper
 
+height, width = [0, 0]
+
 def main(screen):
-    height, width = screen.getmaxyx()
+    nonlocal height, width = screen.getmaxyx()
     index = 1
 	
-    screen.clear()
+    #screen.idcok(False)
+    #screen.idlok(False)
+    #screen.clear()
 
     win = curses.newwin(1, width, height-1, 0)
     header = curses.newwin(1, width, 0, 0)
@@ -17,10 +21,7 @@ def main(screen):
     curses.init_pair(3, curses.COLOR_MAGENTA, curses.COLOR_BLACK)
     curses.init_pair(4, curses.COLOR_YELLOW, curses.COLOR_BLACK)
 
-    banner = '~-> cryptolizard textchannel <-~'
-    header.addstr(0, int(width / 2 - 16), banner, curses.color_pair(3))
-    header.addstr(0, width - 16, 'connected=true', curses.color_pair(4))
-    header.refresh()
+    _header(header, 'robin [0737557200]', 'connected=true')
 
     win.addstr(0, 1, "$ ", curses.color_pair(3))
     win.refresh()
@@ -30,18 +31,23 @@ def main(screen):
         field.edit(exit_on_enter)
         text = field.gather().replace('$ ', '', 1)
         content.addstr(index, 0, "[friend] >> " + text, curses.color_pair(1))
+        content.clrtoeol()
         content.refresh()
-        
-        win.clear()
+
+        index += 1
+        add_test_message(content, index)
+        index += 1
+
+        win.erase()
         win.addstr(0, 1, "$ ", curses.color_pair(3))
         win.refresh()
-        index += 1
-        #add_test_message(content, index)
-        #screen.getch()
+
+        if index > height -4:
+            index = 2
         #_help(content)
 
 def _help(content):
-    content.clear()
+    content.erase()
     index = 1
     content.addstr(index, 0, "commands!", curses.color_pair(3))
     index += 1
@@ -53,6 +59,12 @@ def _help(content):
     index += 1
     content.addstr(index, 0, "commands!", curses.color_pair(3))
     content.refresh()
+
+def _header(header, text, status):
+    banner = '~-> ' + text + ' <-~'
+    header.addstr(0, int(width / 2 - 16), banner, curses.color_pair(3))
+    header.addstr(0, width - 16, status, curses.color_pair(4))
+    header.refresh()
 
 def exit_on_enter(key):
 	if key == 10 or key == 13:
